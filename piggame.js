661733d48ee1afa5a1374c6ae7019dd947278ebc
ronysoumen <43927213+ronysoumen@users.noreply.github.com>
2020-01-12 00:27:42 +0530

    Add files via upload

---
+++ piggame.js
@@ -0,0 +1,248 @@
+
+//document.querySelector('#score-0').textContent=dice;
+/*
+var element = document.querySelector("#current-0")
+if (element) {
+    element.innerHTML = dice
+}
+document.querySelector('#score-0').innerHTML='<em>'dice'<em>';
+*/
+
+/* how to moodify css*/
+/*document.querySelector('.dice').style.display='none';
+var activeplayer=0;
+var value=0;
+var score1=document.getElementById('score-0');
+var score2=document.getElementById('score-1');
+ var current1= document.getElementById('current-0');
+var current2=document.getElementById('current-1');
+//var score=document.getElementById('score-'+activeplayer);
+//var scorenumber=parseInt(score.textContent,10);
+var scorenumber;
+scorenumber=[0,0];
+//var scorenumber2=0;
+
+score1.textContent=0;
+score2.textContent=0;
+current1.textContent=0;
+current2.textContent=0;
+var dicedom=document.querySelector('.dice');
+var isEnter=document.querySelector('.winningScore');*/
+//.required=true;
+//if(isEnter.value===""){
+//document.querySelector('.btn-roll').disabled=true;	
+
+	
+var activeplayer,value,score1,score2,scorenumber,dicedom,isEnter;
+	
+//}
+newgame();
+
+document.querySelector('.btn-roll').disabled=false;
+document.querySelector('.btn-roll').addEventListener("click",function(){
+	//isEnter.required=true;
+
+//var dice=0;
+	if(isEnter.value===""){
+		promptEnterScore();
+	}
+	
+	
+	
+	
+	else{
+	
+dice=Math.floor(Math.random(0,6)*6)+1;
+	
+
+	dicedom.style.display='block';	
+	dicedom.src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/243004/dice-"+dice+".png";
+	
+	if(dice!==1){
+		value+=dice;
+		document.getElementById('current-'+activeplayer).textContent=value;
+	}
+	else{
+		
+		
+		//next player
+		document.getElementById('score-'+activeplayer).textContent=0;
+		document.getElementById('current-'+activeplayer).textContent=0;
+		dicedom.style.display='block';
+		//dicedom.style.display='none';
+		scorenumber[activeplayer]=0;
+		value=0
+		nextplayer();
+		//dicedom.style.display='none';
+		//scorenumber[activeplayer]=0;
+ 
+		//document.getElementById('score-'+activeplayer).textContent=0;
+		
+		//value=0;
+				
+	}
+		
+}
+}
+);
+
+
+
+	
+document.querySelector('.btn-hold').disabled=false;
+
+document.querySelector('.btn-hold').addEventListener("click",function(){
+	console.log(isEnter.value);
+	
+	if(isEnter.value===""){
+		promptEnterScore();
+	}
+	else{
+		errormesage();
+	dicedom.style.display='none';
+	
+	/*if(activeplayer===0){
+		scorenumber1+=value;
+		document.getElementById('score-0').textContent=scorenumber1
+	}
+	else{
+		scorenumber2+=value;
+			document.getElementById('score-1').textContent=scorenumber2;
+	}*/
+	//console.log(value);
+	scorenumber[activeplayer]+=value;
+	//console.log("after"+value);
+	//console.log(scorenumber[activeplayer]);
+	
+	document.querySelector('#score-' +activeplayer).textContent=scorenumber[activeplayer];
+	console.log(document.getElementById('score-'+activeplayer).textContent);
+	
+	if(scorenumber[activeplayer]>=isEnter.value){
+		
+		var s="WINNER!";
+		document.getElementById('score-'+activeplayer).textContent=s
+		;
+		//document.getElementById('score-'+activeplayer).style.fontSize="xx-large";
+		document.getElementById('score-'+activeplayer).style.fontStyle="bold";
+		document.getElementById('score-'+activeplayer).style.color="red";
+
+		document.querySelector('.btn-hold').disabled=true;
+		document.querySelector('.btn-roll').disabled=true;
+		
+		
+	}
+	
+	nextplayer();
+
+	
+	
+}
+});
+
+
+function nextplayer(){
+	
+	//document.getElementById('current-'+activeplayer).textContent=0;
+		//document.getElementById('score-'+activeplayer).textContent=0;
+		
+	
+		if(activeplayer===0){
+			document.querySelector('.player-0-panel').classList.remove('active');
+			//document.querySelector('.player-1-panel').classList.add('active');
+			document.getElementById('current-'+activeplayer).textContent=0;
+			//document.getElementById('score-'+activeplayer).textContent=0;
+			activeplayer=1;
+			document.querySelector('.player-1-panel').classList.add('active');
+		
+		}
+		else if(activeplayer===1){
+		document.querySelector('.player-1-panel').classList.remove('active');
+			//document.querySelector('.player-1-panel').classList.add('active');
+		    document.getElementById('current-'+activeplayer).textContent=0;
+			//document.getElementById('score-'+activeplayer).textContent=0;
+			activeplayer=0;
+			document.querySelector('.player-0-panel').classList.add('active');
+		
+		}
+	
+	value=0;
+	
+	
+	
+}
+
+//button class="btn-new"
+document.querySelector('.btn-new').addEventListener("click",function(){
+	newgame();
+	isEnter.value="";
+	
+	
+	
+});
+
+function newgame(){
+	
+document.querySelector('.dice').style.display='none';
+ activeplayer=0;
+ value=0;
+ score1=document.getElementById('score-0');
+ score2=document.getElementById('score-1');
+ var current1= document.getElementById('current-0');
+var current2=document.getElementById('current-1');
+//var score=document.getElementById('score-'+activeplayer);
+//var scorenumber=parseInt(score.textContent,10);
+//var scorenumber;
+scorenumber=[0,0];
+//var scorenumber2=0;
+
+score1.textContent=0;
+score2.textContent=0;
+current1.textContent=0;
+current2.textContent=0;
+ dicedom=document.querySelector('.dice');
+ isEnter=document.querySelector('.winningScore');
+		document.querySelector('.btn-hold').disabled=false;
+		document.querySelector('.btn-roll').disabled=false;
+	
+	
+	
+}
+
+function promptEnterScore(){
+	alert("Please enter winning score");
+	
+	
+}
+function errormesage(){
+	if(isEnter.value===0){
+	isEnter.value="this is invalid name";
+	}
+	
+}
+
+
+
+/*
+document.getElementById('current-'+activeplayer).textContent=0;
+		document.getElementById('score-'+activeplayer).textContent=0;
+		
+	
+		if(activeplayer===0){
+			document.querySelector('.player-0-panel').classList.remove('active');
+			//document.querySelector('.player-1-panel').classList.add('active');
+			activeplayer=1;
+			document.querySelector('.player-1-panel').classList.add('active');
+			
+		}
+		else if(activeplayer===1){
+		document.querySelector('.player-1-panel').classList.remove('active');
+			//document.querySelector('.player-1-panel').classList.add('active');
+			activeplayer=0;
+			document.querySelector('.player-0-panel').classList.add('active');
+		}
+
+
+
+
+
+*/
